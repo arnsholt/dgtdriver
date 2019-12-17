df65d3b4bc83529825c9debcfe345b3733ccecb4
Arne Skjærholt <arnsholt@gmail.com>
2019-12-17 14:53:08 +0100

    First cut of parsing EEMoves data.

--- src/main/java/org/riisholt/dgtdriver/EEMoves.java
+++ src/main/java/org/riisholt/dgtdriver/EEMoves.java
@@ -1,5 +1,13 @@
 package org.riisholt.dgtdriver;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import java.time.Duration;
+
+import org.riisholt.dgtdriver.game.Role;
+
 public class EEMoves implements DgtMessage {
     public static final byte EE_POWERUP         = 0x6a;
     public static final byte EE_EOF             = 0x6b;
@@ -17,7 +25,66 @@ public class EEMoves implements DgtMessage {
     // 0x40 to 0x5f: two-byte field update message
     // 0x60-69 and 0x70-0x79: three byte clock message
 
-    public EEMoves(byte[] data) {
-        // TODO: Figure out best representation of data and parse it.
+    public final List<EEEvent> events;
+
+    public EEMoves(byte[] data) throws DgtProtocolException {
+        ArrayList<EEEvent> events = new ArrayList<>(data.length);
+        for(int i = 0; i < data.length;) {
+            byte value = data[i];
+            if((0x6a <= value && value <= 0x6f) || (0x7a <= value && value <= 07f) || value == 0x00) {
+                events.add(new SimpleEvent(value));
+                i++;
+            }
+            else if(0x40 <= value && value <= 0x5f) {
+                /* Not sure if this is necessary, but in case we somehow get
+                 * partial data at the end, break if we'd overflow.
+                 */
+                if(i+1 >= data.length)
+                    break;
+                events.add(new FieldEvent((byte) (value & 0x0f), data[i+1]));
+                i += 2;
+            }
+            else if((0x60 <= value && value <= 0x69) || (0x70 <= value && value <= 0x79)) {
+                // As in the case above, break in case of partial trailing data
+                if(i + 2 >= data.length)
+                    break;
+                events.add(new ClockEvent((value & 0x10) == 0x10, (byte) (value & 0x0f), data[i+1], data[i+2]));
+                i += 3;
+            }
+        }
+
+        this.events = Collections.unmodifiableList(events);
+    }
+
+    public static abstract class EEEvent {}
+
+    public static class SimpleEvent extends EEEvent {
+        public final byte type;
+
+        SimpleEvent(byte type) {
+            this.type = type;
+        }
+    }
+
+    public static class FieldEvent extends EEEvent {
+        public final int square;
+        public final Role role;
+
+        FieldEvent(byte piece, byte field) throws DgtProtocolException {
+            square = DgtConstants.dgtCodeToSquare(field);
+            role = DgtConstants.dgtCodeToRole(piece);
+        }
+    }
+
+    public static class ClockEvent extends EEEvent {
+        public final boolean isLeft;
+        public final Duration time;
+
+        ClockEvent(boolean isLeft, byte hours, byte minutes, byte seconds) {
+            this.isLeft = isLeft;
+            this.time = Duration.ZERO.plusHours(hours)
+                                     .plusMinutes(minutes)
+                                     .plusSeconds(seconds);
+        }
     }
 }
