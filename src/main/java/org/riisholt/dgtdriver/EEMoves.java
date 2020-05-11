996159e7fb289873f5c16008f27c5a6df79bdd1c
Arne Skjærholt <arnsholt@gmail.com>
2020-05-11 19:38:12 +0200

    Progress on EEMoves.

--- src/main/java/org/riisholt/dgtdriver/EEMoves.java
+++ src/main/java/org/riisholt/dgtdriver/EEMoves.java
@@ -8,25 +8,100 @@ import java.time.Duration;
 
 import org.riisholt.dgtdriver.game.Role;
 
+/**
+ * Moves transmitted from on-board EEPROM. DGT boards have a built-in EEPROM
+ * (Electrically Erasable Programmable Read Only Memory, a kind of
+ * non-volatile memory) which stores 8kB of data, allowing the recording of
+ * games from a board after the fact, as long as the board was powered when
+ * they were played. The memory is used as a ring buffer, so that incoming
+ * data replaces the oldest data already written.
+ *
+ * @see <a href="https://en.wikipedia.org/wiki/EEPROM">EEPROM</a>
+ * @see <a  href="https://en.wikipedia.org/wiki/Circular_buffer">Ring buffer</a>
+ */
 public class EEMoves implements DgtMessage {
+    /**
+     * When the board is powered, this tag is written (after three NOP tags).
+     */
     public static final byte EE_POWERUP         = 0x6a;
+
+    /**
+     * This tag is always sent at the end of the event stream.
+     */
     public static final byte EE_EOF             = 0x6b;
+
+    /**
+     * This tag is sent when the board detects that the board has eight
+     * pieces on all fields of the first, second, seventh and eighth ranks,
+     * but the position is <em>not</em> the canonical starting position. The
+     * canonical starting position generates {@link EEMoves#EE_BEGINPOS} (or
+     * {@link EEMoves#EE_BEGINPOS_ROT} if rotated).
+     */
     public static final byte EE_FOURROWS        = 0x6c;
+
+    /**
+     * This tag is sent when the board is empty.
+     */
     public static final byte EE_EMPTYBOARD      = 0x6d;
+
+    /**
+     * This tag marks the position of a previous download of the EEPROM.
+     */
     public static final byte EE_DOWNLOADED      = 0x6e;
+
+    /**
+     * This tag is sent when the pieces on the board are in the starting position.
+     */
     public static final byte EE_BEGINPOS        = 0x6f;
+
+    /**
+     * This tag is sent when the pieces on the board are in the starting
+     * position, but rotated so that white is on G and H, and black on A and
+     * B.
+     */
     public static final byte EE_BEGINPOS_ROT    = 0x7a;
+
+    /**
+     * This tag is sent in the position marked by the bus-mode set start game
+     * command (bus mode is not yet implemented by this library).
+     */
     public static final byte EE_START_TAG       = 0x7b;
+
+    /**
+     * Boards equipped with a watchdog timer will have this tag written if the
+     * timer fires, before the normal boot sequence.
+     *
+     * @see <a href="https://en.wikipedia.org/wiki/Watchdog_timer">Watchdog timer</a>
+     */
     public static final byte EE_WATCHDOG_ACTION = 0x7c;
+
+    /**
+     * Reserved for future use.
+     */
     public static final byte EE_FUTURE_1        = 0x7d;
+
+    /**
+     * Reserved for future use.
+     */
     public static final byte EE_FUTURE_2        = 0x7e;
+
     public static final byte EE_NOP             = 0x7f;
     public static final byte EE_NOP2            = 0x00;
     // 0x40 to 0x5f: two-byte field update message
     // 0x60-69 and 0x70-0x79: three byte clock message
 
+    /**
+     * The list of events received from the board.
+     */
     public final List<EEEvent> events;
 
+    /**
+     * Parses a sequence of bytes received from the board into a sequence of
+     * {@link EEEvent EEEvents}.
+     *
+     * @param data The bytes received from the board
+     * @throws DgtProtocolException If a field or role code is invalid
+     */
     public EEMoves(byte[] data) throws DgtProtocolException {
         ArrayList<EEEvent> events = new ArrayList<>(data.length);
         for(int i = 0; i < data.length;) {
@@ -62,8 +137,79 @@ public class EEMoves implements DgtMessage {
         this.events = Collections.unmodifiableList(events);
     }
 
+    /**
+     * Visit the list of events with an {@link Visitor EEMoves.Visitor}
+     * instance.
+     *
+     * @param visitor The visitor implementation.
+     */
+    public void visitEvents(Visitor visitor) {
+        for(EEEvent e: events) {
+            if(e instanceof SimpleEvent) {
+                SimpleEvent simpleEvent = (SimpleEvent) e;
+                switch(simpleEvent.type) {
+                    case EE_POWERUP:
+                        visitor.powerup();
+                        break;
+                    case EE_EOF:
+                        visitor.eof();
+                        break;
+                    case EE_FOURROWS:
+                        visitor.fourRows();
+                        break;
+                    case EE_EMPTYBOARD:
+                        visitor.emptyBoard();
+                        break;
+                    case EE_DOWNLOADED:
+                        visitor.downloaded();
+                        break;
+                    case EE_BEGINPOS:
+                        visitor.initialPosition(false);
+                        break;
+                    case EE_BEGINPOS_ROT:
+                        visitor.initialPosition(true);
+                        break;
+                    case EE_START_TAG:
+                        visitor.startTag();
+                        break;
+                    case EE_WATCHDOG_ACTION:
+                        visitor.watchdogAction();
+                        break;
+                    case EE_NOP:
+                    case EE_NOP2:
+                        // Do nothing for NOP codes.
+                        break;
+                    case EE_FUTURE_1:
+                        visitor.future1();
+                        break;
+                    case EE_FUTURE_2:
+                        visitor.future2();
+                        break;
+                    default:
+                        throw new RuntimeException(String.format("Unknown simple event code 0x%x", simpleEvent.type));
+                }
+            }
+            else if (e instanceof ClockEvent) {
+                ClockEvent clockEvent = (ClockEvent) e;
+                visitor.clockUpdate(clockEvent.time, clockEvent.isLeft);
+            }
+            else if(e instanceof FieldEvent) {
+                FieldEvent fieldEvent = (FieldEvent) e;
+                visitor.fieldUpdate(fieldEvent.square, fieldEvent.role);
+            }
+            else {
+                throw new RuntimeException(String.format("Unknown event class %s", e.getClass().getName()));
+            }
+        }
+    }
+
+    /** Empty class to serve as common supertype for events from EEPROM.*/
     public static abstract class EEEvent {}
 
+    /**
+     * Instances of this class are used to represent events corresponding to
+     * the <tt>EE_*</tt> byte constants in {@link EEMoves}.
+     */
     public static class SimpleEvent extends EEEvent {
         public final byte type;
 
@@ -72,8 +218,18 @@ public class EEMoves implements DgtMessage {
         }
     }
 
+    /** Piece placed on or removed from a field. */
     public static class FieldEvent extends EEEvent {
+        /**
+         * The field updated. Ranges from 0 to 63, where 0 is A1 and 63 is H8,
+         * the same coordinates as used in {@link org.riisholt.dgtdriver.game}
+          */
         public final int square;
+
+        /**
+         * The kind of piece placed on the field, or <tt>null</tt> if a piece
+         * was removed.
+         */
         public final Role role;
 
         FieldEvent(byte piece, byte field) throws DgtProtocolException {
@@ -82,9 +238,17 @@ public class EEMoves implements DgtMessage {
         }
     }
 
+    /**
+     * Clock update. Note that, unlike {@link BWTime} this event only contains
+     * an updated time for the changed side of the clock, not both, and only
+     * the time and nothing else.
+     */
     public static class ClockEvent extends EEEvent {
-        public final boolean isLeft;
+        /** The new time left on the clock. */
         public final Duration time;
+
+        /** <tt>true</tt> if the update applies to the left-hand side. */
+        public final boolean isLeft;
 
         ClockEvent(boolean isLeft, byte hours, byte minutes, byte seconds) {
             this.isLeft = isLeft;
@@ -92,5 +256,33 @@ public class EEMoves implements DgtMessage {
                                      .plusMinutes(minutes)
                                      .plusSeconds(seconds);
         }
+    }
+
+    /**
+     * <p>Visitor class for use with {@link EEMoves#visitEvents(Visitor)} with
+     * empty implementations for all the methods.</p>
+     *
+     * <p>The methods {@link #fieldUpdate(int, Role)} and
+     * {@link #clockUpdate(Duration, boolean)} are called when visiting
+     * {@link FieldEvent} and {@link ClockEvent} events, respectively, while
+     * the rest correspond to {@link SimpleEvent SimpleEvents} containing the
+     * relevant <tt>EE_*</tt> event code; note however that
+     * {@link EEMoves#EE_BEGINPOS} and {@link EEMoves#EE_BEGINPOS_ROT} have
+     * been merged to a single method {@link #initialPosition(boolean)}, with
+     * the parameter set to <tt>true</tt> in the rotated case.</p>
+     */
+    public static class Visitor {
+        public void fieldUpdate(int square, Role role) {}
+        public void clockUpdate(Duration timeLeft, boolean isLeft) {}
+        public void powerup() {}
+        public void eof() {}
+        public void fourRows() {}
+        public void emptyBoard() {}
+        public void downloaded() {}
+        public void initialPosition(boolean rotated) {}
+        public void startTag() {}
+        public void watchdogAction() {}
+        public void future1() {}
+        public void future2 () {}
     }
 }
